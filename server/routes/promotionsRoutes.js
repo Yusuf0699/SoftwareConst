aae94ff710bc40f0af102f4e747069820a602aed
Waleed Khaled Omar Ba dobian <102128756+Waleed-xc@users.noreply.github.com>
2023-07-19 22:56:44 +0800

    adjust
    correction

--- server/routes/promotionsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const promotionsController = require('../controllers/promotionsController');
-// Route to create a new item promotion
-router.post('/promos', promotionsController.createItemPromotion);
-// Route to get all item promotions
-router.get('/viewpromos', promotionsController.getAllItemPromotions);
-
-module.exports = router;
--- server/routes/reserveRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reserveController = require('../controllers/reserveController');
-// Reserve a table
-router.post('/reserve', reserveController.reserveTable);
-// Get all reserved tables
-router.get('/reserved', reserveController.getAllReservedTables);
-
-module.exports = router;
